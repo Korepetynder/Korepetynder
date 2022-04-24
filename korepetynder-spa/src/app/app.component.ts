2bdcdb9ecac7bf667616443c1623c1978b02ab7b
Konrad Krawiec <konrad@krawiec.cc>
2022-04-24 14:44:23 +0200

    Initial roles support + refactoring

--- korepetynder-spa/src/app/app.component.ts
+++ korepetynder-spa/src/app/app.component.ts
@@ -1,24 +1,46 @@
-import { Component, Input } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { InteractionStatus } from '@azure/msal-browser';
+import { filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'korepetynder-spa';
   mobileQuery: MediaQueryList;
   @Input() settingsTab: number = 0;
   maxSettingsTab: number = 2;
 
+  isIframe = false;
+
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private msalBroadcastService: MsalBroadcastService,
+    private authService: MsalService
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.isIframe = window !== window.parent && !window.opener;
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None)
+      )
+      .subscribe(() => {
+        this.checkAndSetActiveAccount();
+      })
   }
 
   ngOnDestroy(): void {
@@ -27,5 +49,18 @@ export class AppComponent implements OnDestroy {
 
   setSelectedTab(id: number): void {
     this.settingsTab = id;
+  }
+
+  private checkAndSetActiveAccount() {
+    /**
+     * If no active account set but there are accounts signed in, sets first account to active account.
+     * To use active account set here, subscribe to inProgress$ first in the component.
+     */
+    let activeAccount = this.authService.instance.getActiveAccount();
+
+    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
+      let accounts = this.authService.instance.getAllAccounts();
+      this.authService.instance.setActiveAccount(accounts[0]);
+    }
   }
 }
