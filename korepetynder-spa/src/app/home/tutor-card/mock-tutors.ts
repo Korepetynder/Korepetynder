6cbca362230b8da6d051ff09ad9454873084cfef
amwms <ania.stawiska@gmail.com>
2022-04-25 02:30:27 +0200

    Displaying tutors with their own details - added mock tutors details

---
+++ korepetynder-spa/src/app/home/tutor-card/mock-tutors.ts
@@ -0,0 +1,8 @@
+import { Tutor } from './tutor';
+
+export const MockTutors: Tutor[] = [
+  { id: 0, name: 'Mr Nice', description: "Hi I am a very good tutor!", contactInfo: "+48 456 475 486" },
+  { id: 1, name: 'Mr Evil', description: "Hi I am a good tutor!", contactInfo: "+48 111 111 111" },
+  { id: 2, name: 'Mr Sun', description: "Hi I am a very sunny tutor!", contactInfo: "+48 433 433 386" },
+  { id: 3, name: 'Mrs Marvel', description: "Hi I am an amazing tutor!", contactInfo: "+48 133 111 331" },
+];
--- korepetynder-spa/src/app/home/tutor-card/tutor-card.component.ts
+++ korepetynder-spa/src/app/home/tutor-card/tutor-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Tutor } from './tutor';
+import { MockTutors } from './mock-tutors';
 
 @Component({
   selector: 'app-tutor-card',
@@ -8,15 +9,28 @@ import { Tutor } from './tutor';
   styleUrls: ['./tutor-card.component.scss']
 })
 export class TutorCardComponent implements OnInit {
-  tutor: Tutor | undefined;
+  tutor: Tutor = MockTutors[0];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getFirstTutor();
+  }
+
+  getFirstTutor(): void {
+    this.tutor = MockTutors[0];
   }
 
   getNextTutor(): void {
+    if (this.tutor == undefined) {
+      this.getFirstTutor();
+    }
+    else {
+      let id = this.tutor.id;
+      id = (id + 1) % 4;
 
+      this.tutor = MockTutors[id];
+    }
   }
 
   addToFavorites(): void {
